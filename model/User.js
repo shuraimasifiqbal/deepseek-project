5c97d66f54a98e4cbaff019b2f4f84da63346206
shuraimasifiqbal <161836058+shuraimasifiqbal@users.noreply.github.com>
2026-01-01 21:19:43 +0500

    Update User.js

--- model/User.js
+++ model/User.js
@@ -1,11 +1,27 @@
+// import mongoose from "mongoose";
+
+// const UserSchema = new mongoose.Schema(
+//   {
+//     _id: { type: String, required: true },
+//     name: { type: String, required: true },
+//     email: { type: String, required: true },
+//     image: { type: String, required: false },
+//   },
+//   { timestamps: true }
+// );
+
+// const User = mongoose.models.User || mongoose.model("User", UserSchema);
+
+// export default User;
+
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    _id: { type: String, required: true },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    image: { type: String, required: false },
+    clerk_id: { type: String},
+    name: { type: String},
+    email: { type: String},
+    image: { type: String},
   },
   { timestamps: true }
 );

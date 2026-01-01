a7c6c7d360654fe60277f3ebdb06e88b8228a1de
shuraim asif <shuraimasifiqbal@gmail.com>
2026-01-01 16:43:42 +0500

    Updated User model

--- model/User.js
+++ model/User.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
-    {
-        _id: {type: string, required: true},
-        name: {type: string, required: true},
-        email: {type: string, required: true}, 
-        image: {type: string, required: false}
-    },
-    {timestamps: true}
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    image: { type: String, required: false },
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model.User || mongoose.model("User" , UserSchema)
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
-export default User; 
+export default User;

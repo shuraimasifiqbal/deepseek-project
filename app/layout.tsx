d7e19f60ac1d5e4a959eb5533570bf19e6e39d79
shuraimasifiqbal <161836058+shuraimasifiqbal@users.noreply.github.com>
2026-01-05 14:33:58 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { AppContextProvider } from "@/context/AppContext";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,6 +27,13 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased`}
       >
+        <Toaster toastOptions={
+          {
+            success: {style: {background: "black" , color: "white"}},
+            error: {style: {background: "black" , color: "white"}}
+            
+          }
+        }/>
         {children}
       </body>
     </html>
